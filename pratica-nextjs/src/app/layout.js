2503c2253e3ec5c9bc755cb1458070c710159bf6
ryan <ryan9821.silva@gmail.com>
2026-04-16 11:11:11 +0000

    configuração basica do site

---
+++ pratica-nextjs/src/app/layout.js
@@ -0,0 +1,25 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Aula 1 ",
+  description: "Aulinha de NextJs slk!!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
